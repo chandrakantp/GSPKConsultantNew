ab013983621dd402ab1e99b15af44d61f9b48cb0
patilchandrakant98 <patilchandrakant98@outlook.com>
2025-01-18 15:23:07 +0530

    Add project files.

---
+++ GSPKConsultantNew/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GSPKConsultantNew.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
